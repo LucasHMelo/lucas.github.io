e2be455b89293c695425f3ee49a6be5150f2f348
LucasHMelo <lucas_melo-10@hotmail.com>
2019-07-12 19:25:24 -0300

    Forms e validações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
-
+import {HttpClientModule } from '@angular/common/http'
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import {ReactiveFormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FuncAddComponent } from './func-add/func-add.component';
@@ -20,7 +22,9 @@ import { FuncGetComponent } from './func-get/func-get.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlimLoadingBarModule.forRoot()
+    SlimLoadingBarModule.forRoot(),
+    AngularFontAwesomeModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/func-add/func-add.component.ts
+++ src/app/func-add/func-add.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, createPlatformFactory } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-func-add',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./func-add.component.css']
 })
 export class FuncAddComponent implements OnInit {
+  adicionarFuncionarioForm: FormGroup;
 
-  constructor() { }
+
+  constructor(private formbuilder: FormBuilder) {
+    this.createForm();
+  }
+    
+    createForm(){
+      this.adicionarFuncionarioForm = this.formbuilder.group({
+        nomeFuncionario: ['', Validators.required],
+        cargo : ['', Validators.required],
+        numeroIdentificador: ['', Validators.required]
+
+      })
+    }
 
   ngOnInit() {
   }
