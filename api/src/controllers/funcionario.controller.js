090b0af809266c1b8e9b22e82ba24e5154bd8edb
LucasHMelo <lucas_melo-10@hotmail.com>
2019-07-23 15:47:07 -0300

    FindbyID

--- api/src/controllers/funcionario.controller.js
+++ api/src/controllers/funcionario.controller.js
@@ -20,12 +20,25 @@ exports.create = (req, res) => {
 };
 
 exports.findAll = (req, res) => {
-    Funcionario.find()
-      .then((funcionarios) => {
-        res.status(200).send(funcionarios);
-      }).catch((err) => {
-        res.status(500).send({ message: 'Erro ao selecionar todos(as) os(as) Funcionários(as)' || err.message });
-      });
-  };
+  Funcionario.find()
+    .then((funcionarios) => {
+      res.status(200).send(funcionarios);
+    }).catch((err) => {
+      res.status(500).send({ message: 'Erro ao selecionar todos(as) os(as) Funcionários(as)' || err.message });
+    });
+};
 
-
+exports.findById = (req, res) => {
+  Funcionario.findById(req.params.id)
+    .then((funcionario) => {
+      if (!funcionario) {
+        return res.status(404).send({message: `Funcionario não encontrado ${req.params.id}` });
+      }
+      res.status(200).send(funcionario);
+    }).catch((err) => {
+      if (err.kind === 'ObjetId') {
+        return res.status(400).send({ message: `Funcionario não encontrado ${req.params.id}` });
+      }
+      res.status(500).send({ message: 'Erro ao selecionar todos(as) os(as) Funcionários(as)' || err.message });
+    });
+};
--- api/src/routes/funcionario.routes.js
+++ api/src/routes/funcionario.routes.js
@@ -5,9 +5,11 @@ const funcionarioController = require('../controllers/funcionario.controller');
 
 // router.get('/', controller.get);
 // router.get('/:id', controller.getById);
-router.get('/findAll', funcionarioController.findAll);
-router.post('/create', funcionarioController.create);
 // router.put('/:id', controller.put);
 // router.delete('/:id', controller.delete);
+
+router.post('/funcionario', funcionarioController.create);
+router.get('/funcionarios', funcionarioController.findAll);
+router.get('/funcionario/:id', funcionarioController.findById);
 
 module.exports = router;
