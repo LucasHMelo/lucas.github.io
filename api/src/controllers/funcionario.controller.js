e7b12d894b1198f43dc76d3c798a36c394c5369a
LucasHMelo <lucas_melo-10@hotmail.com>
2019-07-25 23:48:04 -0300

    CRUD COMPLETO com API Express

--- api/src/controllers/funcionario.controller.js
+++ api/src/controllers/funcionario.controller.js
@@ -59,3 +59,47 @@ exports.findById = (req, res) => {
       res.status(500).send({ message: 'Erro ao selecionar os(as) Funcionários(as) || err.message' });
     });
 };
+
+exports.update = (req, res) => {
+  // Validar os campos
+  if (!req.body.nomeFuncionario) {
+    return res.status(400).send({ message: 'Os campos não podem ser vazios! ' });
+  }
+
+  // Encontrar 'Id' do Funcionário e depois atualizar os dados via 'request':
+  Funcionario.findByIdAndUpdate(req.params.id, {
+    nomeFuncionario: req.body.nomeFuncionario,
+    cargo: req.body.cargo,
+    numeroIdentificador: req.body.numeroIdentificador,
+  }, { new: true })
+    .then((funcionario) => {
+      if (!funcionario) {
+        res.status(404).send({ message: `Funcionário(a) não encontrado(a) ${req.params.id}` });
+      }
+
+      res.status(200).send({ message: 'Funcionário(a) atualizado(a) com sucesso!', funcionario });
+    }).catch((err) => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({ message: `Erro ao encontrar o Id do Funcionário(a) ${req.params.id}` });
+      }
+
+      res.status(500).send({ message: `Erro ao atualizar os dados do(a) Funcionário(a) ${req.params.id}` });
+    });
+};
+
+exports.delete = (req, res) => {
+  Funcionario.findByIdAndDelete(req.params.id)
+    .then((funcionario) => {
+      if (!funcionario) {
+        return res.status(404).send({ message: `Id do Funcionário(a) não encontrado(a) ${req.params.id}` });
+      }
+
+      res.status(200).send({ message: 'Funcionário(a) excluído com sucesso!', funcionario });
+    }).catch((err) => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({ message: `Id do Funcionário(a) não encontrado ${req.params.id}` });
+      }
+
+      return res.status(500).send({ message: `Erro ao excluir Funcionário(a) ${req.params.id}` });
+    });
+};
--- api/src/routes/funcionario.routes.js
+++ api/src/routes/funcionario.routes.js
@@ -11,5 +11,7 @@ const funcionarioController = require('../controllers/funcionario.controller');
 router.post('/funcionario', funcionarioController.create);
 router.get('/funcionarios', funcionarioController.findAll);
 router.get('/funcionario/:id', funcionarioController.findById);
+router.put('/funcionario/:id', funcionarioController.update);
+router.delete('/funcionario/:id', funcionarioController.delete);
 
 module.exports = router;
